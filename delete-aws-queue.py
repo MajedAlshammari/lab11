edee9bea4421594ab03eae28ba2ecfe04a01e188
MajedAlshammari <d12125199@mydit.ie>
2015-11-25 16:47:25 +0000

    Update delete-aws-queue.py

--- delete-aws-queue.py
+++ delete-aws-queue.py
@@ -25,4 +25,4 @@ print secret_access_key
 
 # Set up a connection to the AWS service. 
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
-conn.delete_queue('D12125199')
+conn.delete_queue(q)
