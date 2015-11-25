a1c68ab55761793e8ab471c183716061d51c8ae4
MajedAlshammari <d12125199@mydit.ie>
2015-11-25 16:06:40 +0000

    Update list-aws-queues.py

--- list-aws-queues.py
+++ list-aws-queues.py
@@ -21,6 +21,7 @@ access_key_id = res[:20]
 secret_access_key = res[21:]
 
 print access_key_id
+print secret_access_key
 
 # Set up a connection to the AWS service. 
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
