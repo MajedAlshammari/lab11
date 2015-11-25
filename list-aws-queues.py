48dd32d5ef5b3c28348d9d24c53c032bc0ec9640
MajedAlshammari <d12125199@mydit.ie>
2015-11-25 15:31:16 +0000

    Update list-aws-queues.py

--- list-aws-queues.py
+++ list-aws-queues.py
@@ -9,7 +9,7 @@ from boto.sqs.message import Message
 from boto.sqs.connection import SQSConnection
 from boto.exception import SQSError
 import sys
-
+import requests
 # Get the keys from a specific url and then use them to connect to AWS Service 
 access_key_id = requests.get('http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key')
 secret_access_key = ""
