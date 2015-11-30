e25f6dbd58073135aef3c366b30d9a9b43611027
MajedAlshammari <d12125199@mydit.ie>
2015-11-30 00:15:36 +0000

    Update delete-aws-queue.py

--- delete-aws-queue.py
+++ delete-aws-queue.py
@@ -23,4 +23,4 @@ print secret_access_key
 conn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
 #conn.delete_queue(sys.argv[1])
 
-print 'queue:' sys.argv[1] ' has been deleted'
+print 'queue: %s has been deleted' % sys.argv[1] 
